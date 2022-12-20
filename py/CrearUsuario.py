07c828484b399ca23dd891c1d6d7b0fe1ba1dbd5
Juangarcia8974 <58486504+Juangarcia8974@users.noreply.github.com>
2022-12-20 17:18:25 +0100

    Create CrearUsuario.py

---
+++ py/CrearUsuario.py
@@ -0,0 +1,9 @@
+#este es el Crear Usuario
+Esta funcion es para crear la cuenta del usuario y poder empezar a utilizar la aplicacion.
+
+Condiciones de Satisfaccion:
+    - El usuario tendrá que verificar su correo electrónico
+    - La contraseña tiene que ser de por lo menos 10 caracteres de los cuales 4 tienen que ser numeros o simbolos.
+
+Dependencias:
+    - El correo electronico tiene que existir.
