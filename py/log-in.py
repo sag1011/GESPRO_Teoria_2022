3e05a9a07ffd19dfc23b4b095c8e41b10e2e967b
sag1011 <sag1011@alu.ubu.es>
2022-12-20 18:05:53 +0100

    Create log-in.py

---
+++ py/log-in.py
@@ -0,0 +1,8 @@
+DESARROLLO:
+    -Esta funcion es para poder iniciar sesion en la aplicacion desde un dispositivo.
+
+CONDICIONES DE SATISFACCIÓN:
+    -El usuario y la contraseña tienen que estar correctos.
+
+DEPENDENCIAS:
+    -La cuenta debe existir.
