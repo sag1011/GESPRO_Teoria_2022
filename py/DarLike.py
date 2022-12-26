7f5697cfe746dcfe5b6c84033e3b3ef4ea8dca46
Juangarcia8974 <58486504+Juangarcia8974@users.noreply.github.com>
2022-12-26 17:50:29 +0100

    Update DarLike.py

--- py/DarLike.py
+++ py/DarLike.py
@@ -1,1 +1,9 @@
+DAR LIKE
 
+Esta tarea dara la posibilidad de dar like a una publicacion.
+
+Condiciones de Satisfaccion:
+    - Es la forma de saber si la publicacion le ha gustado a la gente o no.
+
+Dependencias:
+    - Tiene que haber una publicacion a la que pueda dar like.
