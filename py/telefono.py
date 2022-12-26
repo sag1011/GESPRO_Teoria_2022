4e1f4909053a900dfb027cf3c5a565dd88d266f4
sag1011 <sag1011@alu.ubu.es>
2022-12-26 16:58:26 +0100

    Create telefono.py

---
+++ py/telefono.py
@@ -0,0 +1,10 @@
+TELEFONO DE AYUDA
+
+La función consiste en crear un telefono de atención al cliente para que los usuarios puedan estar en contacto las 24 horas del día para cualquier error
+o mal funcionamiento de la app.
+
+CONDICIONES DE SATISFACCIÓN:
+    -Deberemos crear un equipo de telefonistas para atender a las llamadas
+    -El cliente deberá tener una línera telefónica habilitada
+    -Ultima versión actualizada de la aplicación
+    -Tener una cuenta creada en la aplicación para demostrar que es cliente
