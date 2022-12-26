2f9aa305c3d6bea1b1077760e0960ffc5ecbef4c
sag1011 <sag1011@alu.ubu.es>
2022-12-26 17:10:00 +0100

    Update correodeayuda.py

--- py/correodeayuda.py
+++ py/correodeayuda.py
@@ -1,1 +1,13 @@
+CORREO DE AYUDA
+
+El objetivo de esta función es el de crear un email de ayuda para los que prefieran usarle antes que el teléfono
+
+CONDICIONES DE SATISFACCIÓN:
+    -Disponer de un equipo de personas contestando los e-mails de dudas
+    -El usuario debe disponer de una cuenta de CORREO
+
+DEPEDENCIAS
+    -Tener una cuenta de la aplicación creada
+
+
 
