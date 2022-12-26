13d72b68b526958e2e3248a284abecd0fc416a23
Juangarcia8974 <58486504+Juangarcia8974@users.noreply.github.com>
2022-12-26 17:33:44 +0100

    Update CambioTematica.py

--- py/CambioTematica.py
+++ py/CambioTematica.py
@@ -1,1 +1,11 @@
+TAREA DE CAMBIAR IDIOMA:
+
+En este caso la función realiazará una traducción completa del programa para que cualquier persona se instale su idioma como predeterminado en la app.
+
+-CONDICIONES DE SATISFACCIÓN:
+    -El cliente deberá tener instalada la aplicación y deberá estar actualizada.
+    -El cliente deberá saber acceder a la opción de cambiar idioma en el caso de que al iniciar no entienda el texto
+
+-DEPENDENCIAS:
+    -Tener cuenta en la aplicación.
 
