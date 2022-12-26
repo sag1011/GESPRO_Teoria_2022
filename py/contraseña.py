2851fab1e17275d51aed1b7ef1eb3e70984098e3
sag1011 <sag1011@alu.ubu.es>
2022-12-26 17:04:53 +0100

    Create contraseña.py

---
+++ py/contraseña.py
@@ -0,0 +1,10 @@
+RECUPERAR CONTRASEÑA
+
+Esta tarea sirve para recuperar la contraseña en el caso de que el usuario no se acuerde.
+
+CONDICIONES DE SATISFACCIÓN:
+    -Para poder recuperar la contraseña y por lo tanto poder seguir utilizando la cuenta.
+
+DEPENDENCIAS
+    -Se debe dar el correo de la cuenta o el nombre de usuario.
+    -Se debe tener acceso al correo de recuperacion de la cuenta.
