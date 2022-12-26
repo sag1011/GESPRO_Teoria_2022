f22b6f8bf5086afe4bfc5b6439d4126ac4b97e11
sag1011 <sag1011@alu.ubu.es>
2022-12-26 16:50:55 +0100

    Update cambiodeletra.py

--- py/cambiodeletra.py
+++ py/cambiodeletra.py
@@ -1,1 +1,11 @@
+CAMBIO DE TAMAÑO DE LETRA
+ La función consistirá en poder realizar una reducción o un aumento del tamaño de letra en la aplicación.
+ La idea de esta función es poder facilitar la lectura a cualquier persona con problemas de visión.
 
+ CONDICIONES DE SATISFACCIÓN:
+    -Tener la aplicación actualizada para poder realizar cambios en el texto
+    -Conseguir encontrar la opción para el cambio y ajustarla a tu gusto.
+
+DEPENDENCIAS:
+    -Ultima versión de la aplicación instalada
+    -Tener una cuenta creada en la aplicación
