ef6645f106a3b3a912b87ac71bf5e6574ab6c56d
Juangarcia8974 <58486504+Juangarcia8974@users.noreply.github.com>
2022-12-26 17:46:52 +0100

    Create Log-Out.py

---
+++ py/Log-Out.py
@@ -0,0 +1,10 @@
+LOG- OUT
+
+Esta funcion es para poder cerrar la sesion de un dispositivo.
+
+Condiciones de Satisfaccion:
+    - Para evitar posible entrada de otro usuario externo.
+    - Asegura la seguridad en la cuenta.
+
+Depencencias:
+    - La cuenta tiene que estar conectada para poder cerrar sesion.
