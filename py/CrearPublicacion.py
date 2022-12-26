4d89b7fb8a4fe4a0e18116c16cdd0a040b3ac19b
Juangarcia8974 <58486504+Juangarcia8974@users.noreply.github.com>
2022-12-26 17:58:30 +0100

    Create CrearPublicacion.py

---
+++ py/CrearPublicacion.py
@@ -0,0 +1,11 @@
+CREAR PUBLICACION
+
+El usuario podrá crear de cero cualquier publicación que quiera subir a la red social
+
+Condiciones de Satisfaccion:
+    - El usuario tendrá que añadir una foto en el formato correcto.
+    - El usuario tendrá que seguir unas normas de conducta.
+
+Dependencias:
+    - Tener cuenta en la app.
+    - Estar con la sesion iniciada.
