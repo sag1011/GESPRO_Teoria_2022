c869b829cdaa9242852c904b2f3b317abab2348d
Juangarcia8974 <58486504+Juangarcia8974@users.noreply.github.com>
2022-12-27 22:08:01 +0100

    Create CompartirPublicacion.py

---
+++ py/CompartirPublicacion.py
@@ -0,0 +1,10 @@
+COMPARTIR PUBLICACION
+
+El usuario podrá una vez subida una publicación pasarsela por mesnaje a otros seguidores para que la puedan ver directamente.
+
+Condiciones de Satisfaccion:
+    - Disponer de una publicación subida para poder compartirla con otros usuarios.
+
+Dependencias:
+    - Tener algún seguidor para enviar mensaje.
+
