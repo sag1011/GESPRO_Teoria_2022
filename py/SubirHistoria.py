8182f716beb0a66ae253b23f33c4f0ceedf0a880
Juangarcia8974 <58486504+Juangarcia8974@users.noreply.github.com>
2022-12-27 21:56:21 +0100

    Create SubirHistoria.py

---
+++ py/SubirHistoria.py
@@ -0,0 +1,10 @@
+SUBIR HISTORIA
+
+Esta tarea dejara al usuario subir una historia la cual podra ser vista por el resto de usuarios.
+
+Condiciones de Satisfaccion:
+    - Es una manera de subir una publicacion pero en forma de historia.
+
+Dependencias:
+    - El usuario tiene que tener una cuenta.
+
