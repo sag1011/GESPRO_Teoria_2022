ed78706f51f9ec700fe843efb75d1bd109e20862
Juangarcia8974 <58486504+Juangarcia8974@users.noreply.github.com>
2022-12-27 22:04:03 +0100

    Create MandarMensaje.py

---
+++ py/MandarMensaje.py
@@ -0,0 +1,11 @@
+MANDAR MENSAJE
+
+El usuario podrá mandar un mensaje con texto y emoticonos a cualquier seguidor que desee
+
+Condiciones de Satisfaccion:
+    - La otra persona tiene que tener activadas las notificaciones para que se le pueda mandar un mesnaje.
+    - El mensaje no podrá tener mas de 250 caracteres.
+
+Dependencias:
+    - El usuario deber tener seguidores.
+    
