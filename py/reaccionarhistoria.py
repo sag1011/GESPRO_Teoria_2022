c93b1cd0a46131404381c571ceeedf4b93c2cd1f
sag1011 <sag1011@alu.ubu.es>
2022-12-27 18:10:08 +0100

    Create reaccionarhistoria.py

---
+++ py/reaccionarhistoria.py
@@ -0,0 +1,8 @@
+REACCIONAR A HISTORIA
+
+El usuario podrá mostrar con emoticonos o texto su opinión sobre las historias de las personas que sigue
+
+CONDICIONES DE SATISFACCIÓN:
+    -La otra persona debe tener activadas las reacciones para que el usuario pueda interacturar
+DEPENDENCIAS
+    -El usuario tendrá que tener seguidores en la cuenta
