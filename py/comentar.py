0e12770ddd64196bbfbf4f57851b94a17e0a5fc2
sag1011 <sag1011@alu.ubu.es>
2022-12-27 18:14:42 +0100

    Create comentar.py

---
+++ py/comentar.py
@@ -0,0 +1,8 @@
+COMENTAR
+
+Esta tarea dara la posiblidiad de poder comentar una publicacion.
+
+CONDICIONES DE SATISFACCIÓN:
+    -El comentario no podra ser de mas de 250 caracteres incluyendo emojis.
+DEPENDENCIAS:
+    -Tiene que haber una publicacion a la que se pueda comentar.
