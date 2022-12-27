b63c0ab8f74cd7a1ac7a7854b7c645b39c87e320
sag1011 <sag1011@alu.ubu.es>
2022-12-27 18:02:52 +0100

    Update bloquearusuario.py

--- py/bloquearusuario.py
+++ py/bloquearusuario.py
@@ -1,1 +1,8 @@
+BLOQUEAR USUARIO
 
+El usuario podrá tener libertad para decidir que personas no están autorizadas a ver su cuenta y así poder bloquearlas
+
+CONDICIONES DE SATISFACCIÓN:
+    -Que tengas un seguidor que va a pasar a ser bloqueado
+DEPENDENCIAS
+    -Tener una cuenta de la aplicación
