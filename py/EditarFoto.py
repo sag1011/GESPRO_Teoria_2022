b179c80f704ddd53c2b30d56fb27fde4a4472cf9
Juangarcia8974 <58486504+Juangarcia8974@users.noreply.github.com>
2022-12-27 22:00:04 +0100

    Create EditarFoto.py

---
+++ py/EditarFoto.py
@@ -0,0 +1,10 @@
+EDITAR FOTO
+
+El usuario tendrá la posibilida de editar cualquier foto subida previamente
+
+Condiciones de Satisfaccion:
+    - Que la nueva foto tenga modificaciones con respecto a la primera para que se complete.
+
+Dependencias:
+    - Que le usuario ya haya hecho una publicación.
+    
